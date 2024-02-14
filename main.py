af4de5752277b4ec34e912d60c8d2335a8926ebb
Paula Zaidel <paulazaaidel@gmail.com>
2024-02-14 10:41:19 -0300

    install flask

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/hello_world')
+def hello_world():
+    return 'Hello World'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+app.run()
