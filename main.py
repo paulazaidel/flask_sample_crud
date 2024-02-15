988b04c9ba4662963cbdeeed601595f72e638a87
Paula Zaidel <paulazaaidel@gmail.com>
2024-02-15 06:01:54 -0300

    update list.html adding games with delimiters

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/hello_world')
-def hello_world():
-    return render_template('list.html', title="Games")
+class Game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+@app.route('/list')
+def list_games():
+    games = [
+        Game('Tetris', 'Puzzle', 'Atari'),
+        Game('God of War', 'Rock in Slash', 'PS2')
+    ]
+
+    return render_template('list.html', title='Games', games=games)
 
 
 app.run(port=8080)
