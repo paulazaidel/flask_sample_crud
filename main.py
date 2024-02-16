ae2dfcf73aa038d278f05e020596dfda0c119762
Paula Zaidel <paulazaaidel@gmail.com>
2024-02-16 07:40:12 -0300

    add session and refactor url strings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
+app.secret_key = '67ea2d22-6820-47f0-b188-42d59a07aefb'
 
 
 class Game:
@@ -23,6 +24,8 @@ def index():
 
 @app.route('/new')
 def new():
+    if not session['username'] or 'username' not in session:
+        return redirect(url_for('login', redirect=url_for('new')))
     return render_template('post.html', title='New Game')
 
 
@@ -34,12 +37,13 @@ def create():
 
     games.append(Game(name, category, console))
 
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    redirect_page = request.args.get('redirect') or url_for('index')
+    return render_template('login.html', redirect_page=redirect_page)
 
 
 @app.route('/authenticate', methods=['POST'])
@@ -48,9 +52,22 @@ def authenticate():
     password = request.form['password']
 
     if username == 'admin' and password == 'admin':
-        return redirect('/')
+        session['username'] = username
+        flash(f'Welcome {session['username']}!')
 
-    return redirect('/login')
+        redirect_page = request.form['redirect_page']
+        return redirect(redirect_page)
+
+    flash('User not found!')
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session['username'] = None
+
+    flash('Logged out user!')
+    return redirect(url_for('login'))
 
 
 app.run(port=8080, debug=True)
